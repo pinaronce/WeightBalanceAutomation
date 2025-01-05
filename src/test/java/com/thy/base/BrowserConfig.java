29e1534345c4c58a17eb83f362e12be8e5e8aec3
PINAR ONCE (Operasyonel Cozumler Grup Dir. (Yer Operasyonu Cozumleri Md.) - Danisman) <KAFPONCE@THY.COM>
2025-01-05 23:25:18 +0300

    browser fix

--- src/test/java/com/thy/base/BrowserConfig.java
+++ src/test/java/com/thy/base/BrowserConfig.java
@@ -1,11 +1,14 @@
 package com.thy.base;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariOptions;
 
 public class BrowserConfig {
+    private static final Logger logger = LogManager.getLogger(BrowserConfig.class);
 
     public enum BrowserType {
         CHROME, FIREFOX, EDGE, SAFARI
@@ -16,12 +19,29 @@ public class BrowserConfig {
         options.addArguments("--start-maximized");
         options.addArguments("--disable-notifications");
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--incognito");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.setAcceptInsecureCerts(true);
+        
+        logger.info("Chrome options configured");
         return options;
     }
 
     public static FirefoxOptions getFirefoxOptions() {
         FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-notifications");
         options.addArguments("-private");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.setAcceptInsecureCerts(true);
+        
+        logger.info("Firefox options configured");
         return options;
     }
 
@@ -29,12 +49,29 @@ public class BrowserConfig {
         EdgeOptions options = new EdgeOptions();
         options.addArguments("--start-maximized");
         options.addArguments("--disable-notifications");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--incognito");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.setAcceptInsecureCerts(true);
+        
+        logger.info("Edge options configured");
         return options;
     }
 
     public static SafariOptions getSafariOptions() {
         SafariOptions options = new SafariOptions();
         options.setAutomaticInspection(false);
+        options.setAutomaticProfiling(false);
+        options.setUseTechnologyPreview(false);
+        options.setCapability("safari.cleanSession", true);
+        options.setCapability("safari.options.dataDir", "/tmp/safari_data");
+        options.setCapability("safari.options.port", 0);
+        
+        logger.info("Safari options configured");
         return options;
     }
 }
